e76db850db5a062f034b00ef468e88f493f85421
MayThinKyi303 <maythinkyi2003@gmail.com>
2023-12-19 22:40:34 +0630

    upt build fail err

--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -7,7 +7,7 @@ const Introduction = () => {
     <div id='about' className='py-20 md:py-40 text-center'>
       <HeadingText title={'INTRODUCTION'} />
       <p className=' sm:text-xl sm:leading-[60px]  md:leading-[40px] tracking-widest w-[90%] sm:w-[80%] md:w-[60%] mt-16 mb-10 mx-auto'>
-      I specialize in building frontend side of the applications, blending the power of React with the type safety of TypeScript. With a knack for clean code and an eye for UI/UX. Let's innovate and code something awesome together!
+      I specialize in building frontend side of the applications, blending the power of React with the type safety of TypeScript. With a knack for clean code and an eye for UI/UX. Let&apos; s innovate and code something awesome together!
       </p>
       <CustomButton title={'I play with Front End Everyday'} />
     </div>
